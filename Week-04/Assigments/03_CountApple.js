53605e0e31e2b559445aa6f3315a250176452a06
Sumit Sharma <100469632+sumitkumarsharm@users.noreply.github.com>
2025-04-28 13:36:36 +0530

    Delete Week-04 directory

--- Week-04/Assigments/03_CountApple.js
+++
@@ -1,9 +0,0 @@
-function countApple(Apples) {
-  let count = 0;
-  for (let i = 0; i < Apples; i++) {
-    count++;
-  }
-
-  return count;
-}
-console.log(countApple(1000));
--- Week-04/Challenges/loops.js
+++
@@ -1,8 +0,0 @@
-let teas = ["lemon", "Ginger", "masalla", "Green", "Orange", "Rose"];
-let UpdatedTeas = [];
-// itration = loop
-for (let i = 0; i < teas.length; i++) {
-  UpdatedTeas.push(teas[i]);
-}
-
-console.log(UpdatedTeas);
